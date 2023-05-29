bc6bd70edee5f3e1549c7a2dc4fed9860d1c636c
James Anyanwu <jameslecturer@yahoo.com>
2023-05-29 09:42:29 +0100

    Initial Commit

---
+++ GeniusWatch Watch App/GeniusWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeniusWatchApp.swift
+//  GeniusWatch Watch App
+//
+//  Created by James Anyanwu on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GeniusWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
